a87efed19cac92be8782357836c84a094f60a993
MiyawakiGitHub <miyawakimasaomi+github@gmail.com>
2023-11-30 13:21:49 +0000

    issue10_解消

--- demo/src/main/java/com/mysql/App.java
+++ demo/src/main/java/com/mysql/App.java
@@ -1,10 +1,14 @@
 package com.mysql;
 
+import com.mysql.service.HistoryService;
 import com.mysql.service.UsersService;
 public class App {
     public static void main(String[] args) {
-        UsersService usersService = new UsersService();
-        usersService.getUsersAndPrint();
-        usersService.getUsersAndPrintStoredProcedure();
+        // UsersService usersService = new UsersService();
+        // usersService.getUsersAndPrint();
+        // usersService.getUsersAndPrintStoredProcedure();
+
+        HistoryService historyService = new HistoryService();
+        historyService.getHistoryAndPrint();
     }
 }
